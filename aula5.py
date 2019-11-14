f7fe9de69540bd6d5e0534ea4a7372a8f127eadb
Nikolas R. Cristofolini <42762@PROWAY.TREINA>
2019-11-13 23:14:54 -0200

    AULA6

--- aula5.py
+++ aula5.py
@@ -4,34 +4,30 @@
 #4 - Criar tela que lista os carros cadastrados
 #5 - Criar logica que le a lista do arqeuivo texto
 
-from flask import Flask, render_template, request
-from carros
+from flask import Flask, render_template, request, redirect
+from carros import Carro
 
 nome_pagina = "Carros Usados"
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    lista_carros = []
-    with open ('carros.txt', 'r') as arquivo :
-        for l in arquivo:
-            cl = l.strip()split(';')
-            carro = Carro (cl[0], cl[1, cl[2]])
-    return render_template('index2.html', lista = lista_carros)
+    carro = Carro()
+    return render_template('index2.html', lista = carro.lista_todos())
 
 @app.route('/cadastrar')
 def cadastrar():
-    return render_template('cadastrar2', nome = nome_pagina)
+    return render_template('cadastrar2.html', nome = nome_pagina)
 
 @app.route('/salvar')
 def salvar():
     marca = request.args["marca"]
     modelo = request.args["modelo"]
-    categoria = categora.args ["categoria"]
-    ano = ano.args ["ano"]
-
+    categoria = request.args ["categoria"]
+    ano = request.args ["ano"]
+    
     carro = Carro(marca, modelo, categoria, ano)
-    with open ('carros.txt, a') as arquivo :
-        arquivo.write (f'{carro.marca};{carro.modelo};{carro.categoria};{carro.ano}\n')
+    carro.cadastrar_carro(carro)  
 
+    return redirect('/')
 app.run(debug=True)
--- carros.py
+++ carros.py
@@ -1,16 +1,38 @@
 class Carro:
-    def __init__(self, marca, modelo, categoria, ano):
+    def __init__(self, marca ='', modelo='', categoria='', ano=0):
         self.marca = marca
         self.modelo = modelo 
         self.categoria = categoria
         self.ano = ano
 
-        def cadastrar_carro(self):
-    self.marca = input ('Digite sua marca' '\n')
-    self.modelo = input ('Digite o modelo' '\n')
-    self.categoria = input ('Digite a categoria''\n')
-    self.ano = input ('Digite o ano')
-    return f'marca:{self.marca} modelo:{self.modelo} categoria = {self.categoria} ano = {self.ano}' 
+    def lista_todos(self):
+        lista_carros = []
+        with open ('carros.txt', 'r') as arquivo :
+            for l in arquivo:
+                cl = l.strip().split(';')
+                carro = Carro (cl[0], cl[1], cl[2], cl[3])
+                lista_carros.append(carro)
+        return lista_carros
+
+        #--Método para cadastro em arquivo texto
+
+    def cadastrar_carro(self, carro):
+        with open ('carros.txt', 'a') as arquivo :
+        #-- Ao gravar a linha no arquivo texto f' {self}\n', o self, que é a própria classe
+        #-- é convertida em string, a ao ser convertida, é chamado o método __str__ declarado abaixo
+            arquivo.write (f'{self}\n')
+
+        #-- Método de conversão da classe para string
+        #-- Sempre que a classe for convertida para string (ste()) este método será chamado
+    def __str__(self):
+        return f'{self.marca};{self.modelo};{self.categoria};{self.ano}'
+
+    
+        # self.marca = input ('Digite sua marca' '\n')
+        # self.modelo = input ('Digite o modelo' '\n')
+        # self.categoria = input ('Digite a categoria''\n')
+        # self.ano = input ('Digite o ano''\n')
+        # return f'marca:{self.marca} modelo:{self.modelo} categoria = {self.categoria} ano = {self.ano}' 
 
 
          
